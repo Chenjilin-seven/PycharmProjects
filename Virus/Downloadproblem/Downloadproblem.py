dde39cbed21f87d9914fec259803050a2c8e8151
Chenjilin-seven <fangchao@cqupt.edu.cn>
2025-07-28 00:26:26 +0800

    Upload1

---
+++ Virus/Downloadproblem/Downloadproblem.py
@@ -0,0 +1,69 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+import requests
+from urllib.parse import urljoin
+import os
+import sys
+import time
+
+url = "http://ybt.ssoier.cn:8088/"
+def to_URL(img_src, scheme="https"):
+    if not img_src:
+        return None
+    if img_src.startswith("//"):
+        return scheme + ":" + img_src
+    return urljoin(url, img_src)
+option = Options()
+option.add_argument("--user-agent=Mozilla/5.0")
+option.add_argument("--incognito")
+option.add_experimental_option("excludeSwitches", ["enable-automation"])
+option.add_experimental_option("useAutomationExtension", False)
+
+driver = webdriver.Chrome(options=option)
+driver.get(url)
+
+try:
+    driver.execute_script('document.querySelector(\'a[href="login0.php"]\').click();')
+except Exception as e:
+    print(f"login点击失败,原因:{e}")
+    sys.exit(1)
+
+try:
+    driver.execute_script('document.querySelector(\'input[type="text"][name="username"]\').value = arguments[0];', "chenjilin")
+except Exception as e:
+    print(f"users失败,原因:{e}")
+    sys.exit(1)
+
+try:
+    driver.execute_script('document.querySelector(\'input[type="password"][name="password"]\').value = arguments[0];', "Xg520530")
+except Exception as e:
+    print(f"pass失败,原因:{e}")
+    sys.exit(1)
+
+try:
+    driver.execute_script('document.querySelector(\'input[name="login"][type="submit"]\').click();')
+except Exception as e:
+    print(f"login失败,原因:{e}")
+    sys.exit(1)
+
+time.sleep(2)
+findlist = driver.find_elements(By.CSS_SELECTOR, 'td.xlist>a.list2_link')
+option.add_argument("--headless")
+os.makedirs("downloaded_problem", exist_ok=True)
+for item in findlist:
+    title = item.text
+    href = item.get_attribute("href")
+    path = to_URL(href)
+    print(f"{title}: {to_URL(href)}")
+    html = webdriver.Chrome(options=option)
+    html.get(path)
+    with open(f"downloaded_problem\\{title}.html","w",encoding="utf-8") as f:
+        f.write(html.page_source)
+    html.quit()
+driver.quit()
